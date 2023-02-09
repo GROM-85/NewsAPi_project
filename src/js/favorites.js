b975116e89f799c9d2af58052c6e466e39b1c2f8
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-02-09 16:39:16 +0200

    start favorite js file

---
+++ src/js/favorites.js
@@ -0,0 +1,10 @@
+// newsList - refs на li(card)
+//
+
+function addToFavorite(event) {
+  // if (event.target.classList.contains)  якщо по класу
+  if (event.target.dataset.action === 'favorite-btn') {
+  }
+}
+
+newsList.addEventListener('click', addToFavorite);
