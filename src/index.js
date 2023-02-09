c86c0774ecb5dd70ccab594f7bf28884570957ab
GROM-85 <richfron69@gmail.com>
2023-02-09 11:38:25 +0100

    vars added for colors

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ newsFetch.getCategories().then(console.log);
 
 
 // get popular 20 results
-
 newsFetch.getPopularNews().then(console.log)
 
 // get news by query/date 10 results
 newsFetch.query = "apple"
 newsFetch.getNewsByQuery().then(console.log)
+
+// get by category name
+newsFetch.getNewsByCategories().then(console.log)
+
+
