aa5760f600932790d2c6faa5143a22b5d629aab3
Naumetsdmytro <113528509+Naumetsdmytro@users.noreply.github.com>
2023-02-12 00:07:31 +0200

    commit 4

--- src/js/input-actions.js
+++ src/js/input-actions.js
@@ -2,6 +2,8 @@ const buttonEL = document.querySelector('.js-button-search');
 const inputEl = document.querySelector('.form__input-close');
 const svgEl = document.querySelector('.search__icon-hidden');
 
+categoriesOnPageLoad();
+
 buttonEL.addEventListener('click', onInputShow);
 
 function onInputShow(evt) {
@@ -9,13 +11,20 @@ function onInputShow(evt) {
   svgEl.classList.add('search__icon-show');
   buttonEL.classList.add('search__button-hidden');
 }
-window.addEventListener('load', e => {
-  if (e.target.innerWidth >= 767.9) {
+
+function categoriesOnPageLoad() {
+  if (window.matchMedia('(min-width: 769.98px)').matches) {
     inputEl.classList.add('form__input-show');
+    svgEl.classList.add('search__icon-show');
   }
-});
+}
+
 window.addEventListener('resize', e => {
   if (e.target.innerWidth >= 767.9) {
     inputEl.classList.add('form__input-show');
+    svgEl.classList.add('search__icon-show');
+    buttonEL.classList.add('search__button-hidden');
+    return;
   }
+  // inputEl.classList.remove('form__input-show');
 });
--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -1,3 +1,6 @@
+import * as key from './const';
+import * as storage from './storageLogic';
+
 const inputSwitches = document.querySelectorAll('#theme-switch');
 
 inputSwitches.forEach(inputSwitch => {
@@ -9,18 +12,23 @@ function onInputChange(evt) {
   if (evt.currentTarget.checked) {
     document.body.classList.add('darkMode');
     document.body.classList.remove('light');
-    localStorage.setItem('theme', 'darkMode');
+    storage.saveToLocal(key.KEY_THEME, 'darkMode');
   } else {
     document.body.classList.add('light');
     document.body.classList.remove('darkMode');
-    localStorage.setItem('theme', 'light');
+    storage.saveToLocal(key.KEY_THEME, 'light');
   }
 }
 
 function getCurrentTheme(input) {
-  const currentTheme = localStorage.getItem('theme') || 'light';
+  const currentTheme = storage.loadFromLocal('theme') || 'light';
   document.body.className = currentTheme;
   if (currentTheme === 'darkMode') {
     input.checked = true;
   }
+}
+
+// Work with switch text/svg styles//
+
+if (document.body.classList.contains('light')) {
 }
