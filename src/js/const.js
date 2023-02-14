e0a2b2266fd4b576e0947d0cc15231512a7bb188
GROM-85 <richfron69@gmail.com>
2023-02-14 21:06:04 +0100

    KEY_CURR_PAGE

--- src/js/const.js
+++ src/js/const.js
@@ -1,7 +1,7 @@
 export const KEY_THEME = 'theme';
 export const KEY_COLLECTION = 'collection';
 export const KEY_FAVORITE = 'favorite';
-// export const KEY_CURRENT_PAGE = 'current';
+export const KEY_CURRENT_PAGE = 'current';
 export const KEY_READ = 'read';
 export const MAXLENGHT = 110;
 export const READ = false;
